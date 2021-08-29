1cd1e123273f4c1338d164539419a1b00e87445a
Sunil <sunil_karmakar@ymail.com>
2021-08-29 18:15:39 +0600

    Working with Routing
    1. Create Employee List Component.
    2. Create Employee Create Component.
    3. Create Routing Path in Routing.ts
    4. Create Navigation Menu in app.component.html

--- Angular6Project/src/app/app-routing.module.ts
+++ Angular6Project/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { ListEmployeeComponent } from './list-employee/list-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'employess', pathMatch: 'full' },
+  { path: 'employess', component: ListEmployeeComponent },
+  { path: 'create', component: CreateEmployeeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular6Project/src/app/app.module.ts
+++ Angular6Project/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListEmployeeComponent } from './list-employee/list-employee.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      ListEmployeeComponent,
+      CreateEmployeeComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
